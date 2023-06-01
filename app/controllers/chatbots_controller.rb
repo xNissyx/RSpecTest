e32cc90f09cd3a0c085708853fd1a081eabc0d16
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-06-01 18:44:45 +0000

    Ajax通信のCSRF対策の記述追加

--- app/controllers/chatbots_controller.rb
+++ app/controllers/chatbots_controller.rb
@@ -1,7 +1,7 @@
 class ChatbotsController < ApplicationController
     #csrf tokenの問題で追記コード
-    protect_from_forgery
-  
+    # protect_from_forgery
+
     def create
       input = params[:input]
       client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
@@ -15,9 +15,14 @@ class ChatbotsController < ApplicationController
           max_tokens: 200,  # 応答の長さを指定
         },
       )
-  
+
       respond_to do |format|
         format.json { render json: { response: response.dig("choices", 0, "message", "content") } }
       end
+      # respond_toメソッドは、一つのアクションから複数の形式（HTML、JSON、XMLなど）でレスポンスを返す場合に利用します。
+      # 例えば、ブラウザからのリクエストに対してはHTMLを返し、APIからのリクエストに対してはJSONを返す、といったケースで使用します。
+      # response.dig("choices", 0, "message", "content") の部分は、response というハッシュから "choices" の 0 番目の要素の "message" の "content" を取り出すことを意味しています。
+      # dig メソッドは、ハッシュや配列の中を掘り進んで指定した位置の値を取り出す Ruby のメソッドです。
+      # OpenAIのAPIからのレスポンスは、一般にネストされた構造を持つハッシュとして返されます。そのため、dig メソッドを使用して、必要な情報（この場合は AI の生成したテキスト）を取り出しています。
     end
 end
